4b7b4e373ca597947fd329cb759f211e688bcdb4
AmauriBlanco <blancoamauri@gmail.com>
2022-09-28 10:11:04 -0300

    semana 5 - aula 7

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmConfigService } from './database/typeorm-config';
+import { ServicosModule } from './servicos/servicos.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    ServicosModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
